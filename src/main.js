5d146931ebd29aacbcf064268a9e8e431400d478
Zara Yakob <buscasnaz@gmail.com>
2020-11-01 23:23:43 +0900

    Add env

--- src/main.js
+++ src/main.js
@@ -8,14 +8,14 @@ import firebase from 'firebase'
 Vue.config.productionTip = false
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnEA_-Gnc-DzQWnGuKaTNg3y0iGtsFj7w",
-  authDomain: "flocker-92eff.firebaseapp.com",
-  databaseURL: "https://flocker-92eff.firebaseio.com",
-  projectId: "flocker-92eff",
-  storageBucket: "flocker-92eff.appspot.com",
-  messagingSenderId: "963950173250",
-  appId: "1:963950173250:web:221432c9ce0a919f8c21db",
-  measurementId: "G-GQ5C6M47WT"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGEING_SENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 }
 
 firebase.initializeApp(firebaseConfig)

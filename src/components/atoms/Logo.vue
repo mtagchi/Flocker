fe932fdcffafa49a2ed1c7a1fc7c253eeafb831b
Zara Yakob <buscasnaz@gmail.com>
2020-11-03 14:53:38 +0900

    Update logo

--- src/components/atoms/Logo.vue
+++ src/components/atoms/Logo.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="d-flex align-center mr-3">
-    <v-img
-      alt="Vuetify Logo"
-      class="shrink mr-1 d-none d-sm-flex"
-      contain
-      src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-      transition="scale-transition"
-      width="40"
-    />
-    <span class="logo unselectable font-weight-bold headline brown--text text--darken-4">Flocker</span>
-  </div>
+  <router-link :to="{ name: 'Home' }" class="text-decoration-none">
+    <div class="d-flex align-center mr-3">
+      <v-img
+        alt="Flocker"
+        class="shrink mr-2 d-none d-sm-flex"
+        contain
+        src="/favicon/flocker.png"
+        transition="scale-transition"
+        width="40"
+        height="40"
+      />
+      <span class="logo unselectable font-weight-bold headline brown--text text--darken-3">Flocker</span>
+    </div>
+  </router-link>
 </template>
 
 <script>

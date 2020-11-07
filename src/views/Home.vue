0da4beabbaaa7aba7f90b28beafe98d6646da5ee
Zara Yakob <buscasnaz@gmail.com>
2020-11-07 17:47:28 +0900

    Create cta area layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <v-container fluid>
-    <v-row wrap class="cta">
-      <v-col align="center">
-        <h1>Online to Offline</h1>
-        <p>FlockerはTwitterユーザーのための飲み会企画サービスです</p>
-        <v-btn color="primary">飲み会を開く</v-btn>
+    <v-row wrap align="center" class="cta">
+      <v-col cols="6" align="center" class="cta-left">
+        <v-row justify="center">
+          <h1>Online to Offline</h1>
+          <p>FlockerはTwitterユーザーのための飲み会企画サービスです</p>
+          <v-btn width="168" color="primary">飲み会を開く</v-btn>
+        </v-row>
       </v-col>
-      <v-col align="center">
-        <span>aaa</span>
+      <v-col cols="6" align="center" class="cta-right">
+        <v-img
+          src="/img/main-image.svg"
+          width="400"
+        />
       </v-col>
     </v-row>
   </v-container>
@@ -30,6 +35,9 @@ export default {
 @media screen and (max-width: 768px) {
   .cta {
     flex-direction: column;
+  }
+  .cta-left, .cta-right {
+    max-width: 500px;
   }
 }
 </style>

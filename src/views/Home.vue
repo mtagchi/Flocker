456584fc67778c5c83db6ca3d15cb6e110cabfad
Zara Yakob <buscasnaz@gmail.com>
2020-11-08 23:31:37 +0900

    Add latest events list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,9 @@
   <v-container fluid>
     <v-row wrap align="center" class="cta">
       <v-col cols="6" align="center" class="cta-left">
-        <v-row justify="center">
-          <h1>Online to Offline</h1>
-          <p>FlockerはTwitterユーザーのための飲み会企画サービスです</p>
-          <v-btn width="168" color="primary">飲み会を開く</v-btn>
-        </v-row>
+        <h1>Online to Offline</h1>
+        <p>FlockerはTwitterユーザーのための飲み会企画サービスです</p>
+        <v-btn width="168" color="primary">飲み会を開く</v-btn>
       </v-col>
       <v-col cols="6" align="center" class="cta-right">
         <v-img
@@ -15,6 +13,7 @@
         />
       </v-col>
     </v-row>
+    <h2 class="latest-events-title px-5 d-flex align-center justify-space-between">新着イベント</h2>
   </v-container>
 </template>
 
@@ -38,6 +37,14 @@ export default {
   }
   .cta-left, .cta-right {
     max-width: 500px;
+  }
+}
+.latest-events-title {
+  &:before, &:after {
+    content: "";
+    width: min(100px, 18%);
+    height: 1px;
+    background: rgb(0, 0, 0, 0.87);
   }
 }
 </style>
